3a4d676fad3f5123da388c47b747ed02d8577fbe
michaelDonchenko <michael1994d@gmail.com>
2021-02-11 17:42:43 +0200

    express server connected to DB

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./database/connectDB')
+dotenv.config()
+
+app.use(cors())
+
+//Init middleware
+app.use(express.json({ limit: '5mb' }))
+
+// app.use('/api', require(''))
+
+//Databae connection
+connectDB()
+
+//Port listener
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`The app listening on port ${PORT}`)
+})
